7a167175097dedef380544d689645dc8429bf624
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-03-30 23:51:16 +0100

    Time: 68 ms (65.23%), Space: 43.5 MB (68.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,7 +3,23 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-let sorted = nums.sort((a,b) => a-b)
-let length = sorted.length
-return length % 2 === 0 ? nums[length / 2] : nums[(length - 1) / 2]
+// let sorted = nums.sort((a,b) => a-b)
+// let length = sorted.length
+// return length % 2 === 0 ? nums[length / 2] : nums[(length - 1) / 2]
+    
+    // Boyer–Moore majority vote algorithm
+    
+  let count = 0;
+  let majNum = 0;
+  let length = nums.length;
+  for (let i = 0; i < length; i++) {
+    if (!count) {
+      majNum = nums[i];
+      count = 1;
+    } else {
+      count += (nums[i] === majNum ? 1 : -1);
+    }
+  }
+  return majNum;
+    
 };
