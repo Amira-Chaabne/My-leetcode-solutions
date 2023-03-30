8256d1f67b33a6ab6c9fcae53531f563f09a3fce
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-03-31 00:00:03 +0100

    Time: 67 ms (69.70%), Space: 46.9 MB (5.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -8,18 +8,36 @@ var majorityElement = function(nums) {
 // return length % 2 === 0 ? nums[length / 2] : nums[(length - 1) / 2]
     
     // Boyer–Moore majority vote algorithm
+ // let count = 0;
+  // let majNum = 0;
+  // let length = nums.length;
+  // for (let i = 0; i < length; i++) {
+    // if (!count) {
+      // majNum = nums[i];
+      // count = 1;
+    // } else {
+      // count += (nums[i] === majNum ? 1 : -1);
+    // }
+  //}
+  // return majNum;
     
-  let count = 0;
-  let majNum = 0;
-  let length = nums.length;
-  for (let i = 0; i < length; i++) {
-    if (!count) {
-      majNum = nums[i];
-      count = 1;
-    } else {
-      count += (nums[i] === majNum ? 1 : -1);
+        // Trying the fastest from discussion
+    let a = nums.sort();
+    let arr= []
+    let count = 1
+    let max = 0;
+    for(i = 0; i< a.length; i++){
+        if(a[i]==a[i+1]){
+            count++
+        }
+        else{
+            count=1;
+        }
+        if(max<count){
+            max = count;
+            arr.push(a[i])
+        }
     }
-  }
-  return majNum;
+    return arr[arr.length-1];
     
 };
