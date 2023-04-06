7df7cd932234b18a8bfb5a06cc4e80ff5fc7998f
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-06 06:11:19 +0100

    Time: 60 ms (75.47%), Space: 44 MB (10.86%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -5,15 +5,27 @@
  */
 var numJewelsInStones = function(jewels, stones) {
    // let myJewels = 0
-   // for(stone of stones){
+   // for(let stone of stones){
      //  jewels.includes(stone) && myJewels++
     // } 
     // return myJewels
     
+    // let myJewels = 0
+    // let setJewels = new Set(jewels.split(""))
+    // for(let stone of stones){
+       // setJewels.has(stone) && myJewels++
+    //  } 
+    // return myJewels
+    
+    const map = {}
     let myJewels = 0
-    let setJewels = new Set(jewels.split(""))
-    for(stone of stones){
-       setJewels.has(stone) && myJewels++
-     } 
+    
+    for (let jewel of jewels) {
+        map[jewel] = true
+    }
+    
+    for (let stone of stones) {
+        map[stone] ? myJewels++ : null
+    }
     return myJewels
 };
