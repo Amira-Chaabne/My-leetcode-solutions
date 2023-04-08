1f3aa1f69708ae4e27c392afaadd16d2c049037e
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-08 06:51:07 +0100

    Time: 66 ms (61.31%), Space: 42.7 MB (16.58%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -4,16 +4,23 @@
  * @return {number}
  */
 var arithmeticTriplets = function(nums, diff) {
-  let obj = {};
-  let res = 0;
-  
-  for(let i=0;i<nums.length;i++){
-    obj[nums[i]] = true;
-    let curr = nums[i]
-        if(obj[curr-diff] && obj[curr - diff*2]){
-          res++
-      }
-  }
+  // let obj = {};
+  // let res = 0;
+  // for(let i=0;i<nums.length;i++){
+    // obj[nums[i]] = true;
+    // let curr = nums[i]
+       // if(obj[curr-diff] && obj[curr - diff*2]){
+         // res++
+      // }
+  // }
+  // return res;
     
-  return res;
+  const set = new Set()
+  let res = 0
+  for (let num of nums) {
+    set.add(num)
+    if (set.has(num - diff) && set.has(num - 2 * diff)) res++
+  }
+  return res
+    
 };
