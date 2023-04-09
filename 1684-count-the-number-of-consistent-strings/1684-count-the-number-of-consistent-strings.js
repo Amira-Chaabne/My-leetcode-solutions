319a9c0256e81d8704101d146ca2fde54050975e
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-09 09:56:48 +0100

    Time: 114 ms (38.46%), Space: 50.7 MB (87.23%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    let count = 0;
+    allowed = new Set(allowed)
+    
+    // Check if the word is allowed
+    function isAllowed(word){
+        for(let w of word)
+            if(!allowed.has(w)) return false
+        return true
+    }
+    
+    // return count of allowed words
+   for(let ws of words) count += isAllowed(ws)
+    return count
+    
+};
