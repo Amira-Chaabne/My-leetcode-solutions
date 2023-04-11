36159eddd1afe9829077bb080bfda2aa4a3f248a
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-11 07:04:56 +0100

    Time: 59 ms (56.57%), Space: 41.4 MB (93.74%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.js
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.js
@@ -3,12 +3,21 @@
  * @return {character}
  */
 var repeatedCharacter = function(s) {
-    let obj = new Set();
+    // let obj = new Set();
     
-    for(let i = 0; i <= s.length; i++){
-        let curr = s[i];
-        if (obj.has(curr)) return curr;
-        else obj.add(curr);
+    // for(let i = 0; i <= s.length; i++){
+        // let curr = s[i];
+        // if (obj.has(curr)) return curr;
+        // else obj.add(curr);
+    // }
+    
+    const obj = {};
+    for(let i of s) {
+		obj[i] = obj[i] + 1 || 1;  
+        
+        if(obj[i] == 2) {
+            return i;
+        }
     }
     
 };
