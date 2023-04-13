03c687568e618182c4c55428ed79672739f91d60
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-13 05:10:44 +0100

    Time: 58 ms (71.51%), Space: 42.1 MB (69.12%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -3,11 +3,21 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
-    let obj = {}
-    let uniqLength = [];
-    for( let n of nums){
-        obj[n] = obj[n]+1 || 1
-    }
-    let uniq = Object.keys(obj).filter(key => obj[key] === 1);
-    return uniq.reduce((a,b) => Number(a) + Number(b), 0)
+    // let obj = {}
+    // let uniqLength = [];
+    // for( let n of nums){
+       // obj[n] = obj[n]+1 || 1
+    // }
+    // let uniq = Object.keys(obj).filter(key => obj[key] === 1);
+    // return uniq.reduce((a,b) => Number(a) + Number(b), 0)
+
+ const obj = {}
+ let sumUniq = 0
+ for (let n of nums){
+    obj[n] = obj[n]+1 || 1
+    if(obj[n] === 1) sumUniq += n
+    else if (obj[n] === 2) sumUniq -= n
+ }
+ return sumUniq
+
 };
