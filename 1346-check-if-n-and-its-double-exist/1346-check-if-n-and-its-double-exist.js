36337da2c5f7ed9fd3c98d685da7edd4c454d866
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-15 20:46:25 +0100

    Time: 59 ms (77.89%), Space: 43.9 MB (39.75%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,15 +3,25 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    for(let i=0; i<arr.length; i++){
-        for(let j=0; j<arr.length; ++j){
-            console.log({i, j}) // check this
-            if (i != j) {
-                if (arr[i] == 2 * arr[j]) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false
+    // So bad solution :/ , nested loops with time complexity of O(n^2)
+     // for(let i=0; i<arr.length; i++){
+         // for(let j=0; j<arr.length; ++j){
+            // console.log({i, j}) // check this
+             // if (i != j) {
+                // if (arr[i] == 2 * arr[j]) {
+                    // return true;
+                 // }
+             // }
+         // }
+     // }
+     // return false
+    
+   	const set = new Set();
+	for (let value of arr) {
+		if (set.has(2 * value) || set.has(value / 2)) {
+			return true;
+		}
+		set.add(value);
+	}
+	return false;
 };
