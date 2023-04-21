188b3c209c7bf26df9fccafb8ce1b60dff266ae0
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-21 21:08:39 +0100

    Time: 61 ms (71.40%), Space: 44.6 MB (29.35%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    const lengths = sentences.map(sentence => {
-        return sentence.split(" ").length
-    })
-    return Math.max(...lengths)
+    //const lengths = sentences.map(sentence => {
+        //return sentence.split(" ").length
+    //})
+    //return Math.max(...lengths)
+    
+    let count = 0;
+    for( let sentence of sentences){
+        let wordsLengths = sentence.split(" ")
+        count = Math.max(count, wordsLengths.length)
+    }
+    return count
 };
