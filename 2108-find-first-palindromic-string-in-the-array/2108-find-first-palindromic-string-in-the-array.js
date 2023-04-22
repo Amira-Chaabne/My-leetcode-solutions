83892c259eff813ec188e928815e86faf83b60f7
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-22 08:31:37 +0100

    Time: 127 ms (8.43%), Space: 51.1 MB (5.06%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -3,13 +3,27 @@
  * @return {string}
  */
 var firstPalindrome = function(words) {
+    //let firstPalindromic = "";
+    //for ( let word of words){
+       // const reversed = word.split("").reverse().join("")
+        //if (word === reversed){
+         //   firstPalindromic = word
+       //     break;
+     //   }
+    //}
+    //return firstPalindromic
+    
+    //with a recursion : This solution is not the best one and will be really slow if the String is very long
     let firstPalindromic = "";
+    function reverseString(word) {
+        return (word === '') ? '' : reverseString(word.substr(1)) + word.charAt(0);
+    }
+    
     for ( let word of words){
-        const reversed = word.split("").reverse().join("")
-        if (word === reversed){
+        if (word === reverseString(word)){
             firstPalindromic = word
             break;
-        }
+       }
     }
     return firstPalindromic
 };
