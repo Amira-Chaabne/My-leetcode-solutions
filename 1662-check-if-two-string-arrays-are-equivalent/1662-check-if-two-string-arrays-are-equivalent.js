7e3661d685b688ad52b0e1f2a1389587c467da62
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-24 20:43:23 +0100

    Time: 43 ms (99.25%), Space: 42.2 MB (39.44%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -3,7 +3,11 @@
  * @param {string[]} word2
  * @return {boolean}
  */
+//var arrayStringsAreEqual = function(word1, word2) {
+    // return word1.join("") === word2.join("");
+//};
+
+const concatArray = (arr) => arr.reduce((acc,a) => acc+a)
 var arrayStringsAreEqual = function(word1, word2) {
-    return word1.join("") === word2.join("");
-  
+    return concatArray(word1) === concatArray(word2)
 };
