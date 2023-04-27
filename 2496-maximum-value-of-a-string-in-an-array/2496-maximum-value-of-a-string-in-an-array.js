a32f2369d59e3e25c2f169676bf50858da271c89
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-27 08:56:36 +0100

    Time: 60 ms (50.70%), Space: 41.6 MB (86.71%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.js
@@ -3,10 +3,20 @@
  * @return {number}
  */
 var maximumValue = function(strs) {
-  let obj = {}
-  for(let str of strs){
-    if(/^\d+$/.test(str)) obj[str] = +str
-     else obj[str] = str.length
-  }
-  return Math.max(...Object.values(obj))
+  //let obj = {}
+  //for(let str of strs){
+    //if(/^\d+$/.test(str)) obj[str] = +str
+     //else obj[str] = str.length
+  //}
+  //return Math.max(...Object.values(obj))
+  
+  
+  
+    let result = 0;
+    const regex = /^\d+$/;
+    for (let i = 0; i < strs.length; ++i) {
+        result = Math.max(result, regex.test(strs[i]) ? +strs[i] : strs[i].length);
+    }
+    return result;
+
 };
