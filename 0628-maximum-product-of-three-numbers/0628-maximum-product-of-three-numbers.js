211c822198ca4385811173df1660d4b0454357b4
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-04-30 11:27:58 +0100

    Time: 107 ms (74.91%), Space: 47.4 MB (18.47%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -4,7 +4,7 @@
  */
 var maximumProduct = function(nums) {
   nums.sort((a,b) => a - b)
-  prod1 = nums[nums.length -1] * nums[nums.length -2] * nums[nums.length -3]
-  prod2 = nums[0] * nums[1] * nums[nums.length -1]
-  return Math.max(prod1, prod2)
+  product1 = nums[nums.length -1] * nums[nums.length -2] * nums[nums.length -3]
+  product2 = nums[0] * nums[1] * nums[nums.length -1]
+  return Math.max(product1, product2)
 };
