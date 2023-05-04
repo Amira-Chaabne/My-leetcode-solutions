b9dabee103e791c58fd61bd95e8f689e45a5610c
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-05-04 22:30:38 +0100

    Time: 63 ms (48.48%), Space: 44.2 MB (14.27%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,11 +3,14 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
+  // Create an object with values number of occurence of its key
     let obj = {}
     for(let a of arr){
       obj[a] ? obj[a]++ : obj[a] = 1
     }
+  // Get an array of values of the object
   let values = Object.values(obj)
+  // Create a unique arr and compare it to the initial ( array of values )
   return [...new Set(values)].length === values.length
 
 };
