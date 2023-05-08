76ab15451681f884c4ce32efa0ab7f189c0daff0
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-05-08 22:19:14 +0100

    Time: 54 ms (98.87%), Space: 43.9 MB (50.42%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -4,13 +4,13 @@
  */
 var addDigits = function(num) {
   // Recursive solution
-    num = num.toString();
-    let newNumber = 0;
-    for (let n of num) {
-        newNumber += Number(n);
-    }
-    if (newNumber.toString().length === 1) return newNumber;
-    return addDigits(newNumber);
+    //num = num.toString();
+    //let newNumber = 0;
+    //for (let n of num) {
+      //  newNumber += Number(n);
+    //}
+    //if (newNumber.toString().length === 1) return newNumber;
+    //return addDigits(newNumber);
   
   // Mathy solution
   return 1 + (num - 1) % 9; // Still don't know how xDD
