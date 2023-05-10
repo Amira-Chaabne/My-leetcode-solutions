65f5bee37bd29c813f3377f3379d0283659dc53f
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-05-10 07:35:37 +0100

    Time: 95 ms (65.12%), Space: 49.1 MB (33.41%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -5,12 +5,23 @@
  */
 var findDifference = function(nums1, nums2) {
   // the first, not thinking, solution xDD
-    const distinctNum1 = [...new Set(nums1)]
-    const distinctNum2 = [...new Set(nums2)]
-    const res = [[], []]
+    //const distinctNum1 = [...new Set(nums1)]
+    //const distinctNum2 = [...new Set(nums2)]
+    //const res = [[], []]
+  //distinctNum1.map(el => distinctNum2.includes(el) ? null : res[0].push(el))
+  //distinctNum2.map(el => distinctNum1.includes(el) ? null : res[1].push(el))
+  //return res
+  
+  // With new Set
+    
+    nums1 = new Set(nums1)
+    nums2 = new Set(nums2)
 
-  distinctNum1.map(el => distinctNum2.includes(el) ? null : res[0].push(el))
-  distinctNum2.map(el => distinctNum1.includes(el) ? null : res[1].push(el))
- 
-  return res
+    for (let item of nums1){
+        if (nums2.has(item)) {
+            nums1.delete(item)
+            nums2.delete(item)
+        }
+    }
+    return [Array.from(nums1),Array.from(nums2)] 
 };
