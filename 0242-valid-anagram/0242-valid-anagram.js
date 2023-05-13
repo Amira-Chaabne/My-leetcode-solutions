30fac02dff5b6a1bbb4739565ee05015b157cea3
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-05-13 22:35:42 +0100

    Time: 106 ms (18.95%), Space: 48.4 MB (16.00%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -8,17 +8,7 @@ var isAnagram = function(s, t) {
   if(s.length != t.length) return false
   
   // One line solution
-  return s.split('').sort().join('') === t.split('').sort().join('');
-  
-  // Solution with new map
-  // let map = new Map()
-  
-  // for(let c of s){
-     // map.set(c, 1)
-    // t.includes(c) && map.delete(c)
-    // }
-  
-  // return map.size === 0
+   return s.split('').sort().join('') === t.split('').sort().join('');
 };
 
  
