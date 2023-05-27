6218a46201db349ab3e69656bd15c8eb27ef4e1f
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-05-27 21:08:56 +0100

    Time: 129 ms (62.44%), Space: 53.7 MB (53.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,11 +3,22 @@
  * @return {string[][]}
  */
 function groupAnagrams(strs){
-    const wordsMap = strs.reduce((map, str) => {
-        const sortedChars = [...str].sort().join('');
-        map[sortedChars] = (map[sortedChars] || []).concat(str);
-        return map;
-    }, {});
+    //const wordsMap = strs.reduce((map, str) => {
+       // const sortedChars = [...str].sort().join('');
+       // map[sortedChars] = (map[sortedChars] || []).concat(str);
+    //    return map;
+  //  }, {});
+   // return Object.values(wordsMap);
+      let map = {}
 
-    return Object.values(wordsMap);
+  
+  for(let s of strs){
+    const key = s.split("").sort().join("")
+    if(map[key]){
+      map[key].push(s)
+    }else{
+      map[key] = [s]
+    }
+  }
+  return Object.values(map)
 }
