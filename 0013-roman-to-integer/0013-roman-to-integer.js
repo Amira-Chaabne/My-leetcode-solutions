695c5d1a351b5ff1f731fe36ef1575cd6e4db63f
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-06-02 22:24:37 +0100

    Time: 124 ms (75.62%), Space: 46 MB (98.86%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  let nums = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+};
+    let res = 0;
+    for(let i = 0; i < s.length; i++){
+        nums[s[i]] < nums[s[i+1]] ? res -= nums[s[i]]: res += nums[s[i]]
+    }
+    return res
+};
