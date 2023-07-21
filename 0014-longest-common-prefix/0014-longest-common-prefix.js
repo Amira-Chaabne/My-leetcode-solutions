79cf93fb600d3399bbb28abf082999d5287a759f
Amira-Chaabane <61826750+Amira-Chaabne@users.noreply.github.com>
2023-07-21 12:03:44 +0100

    Time: 68 ms (32.97%), Space: 41.6 MB (96.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,9 +4,7 @@
  */
 var longestCommonPrefix = function (strs) {
   for (let i = 0; i <= strs[0].length; i++) {
-        // Check if this character is present in the same position of every string
         if (!strs.every((el) => el[i] === strs[0][i])) {
-            // If not, return the string up to and including the previous character
             return strs[0].slice(0, i);
         }
     }
