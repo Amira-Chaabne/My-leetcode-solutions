134e4deaa4c397cc43267568e5edbd294cedc751
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-08-28 08:33:19 +0100

    Time: 60 ms (55.91%), Space: 43.7 MB (56.90%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -3,6 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
+/**
+// Brut force
 var targetIndices = function(nums, target) {
   let res = []
 let sortedArr = nums.sort((a, b) => a - b )
@@ -11,3 +13,38 @@ let sortedArr = nums.sort((a, b) => a - b )
   }
   return res
 };
+ */
+// With binary search
+function targetIndices(nums, target){
+    nums.sort((a,b)=>a-b);
+    var res = [];
+    var lower = binarySearch(nums, target, true);
+    var upper = binarySearch(nums, target, false);
+    for (var i = lower; i <= upper; i++) {
+        res.push(i);
+    }
+    return res;
+}
+
+function binarySearch(nums, target, isLeft) {
+    var left = 0;
+    var right = nums.length - 1;
+    var mid = 0;
+    var res ;
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (target > nums[mid]) {
+            left = mid + 1;
+        } else if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            res = mid;
+            if (isLeft) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return res;
+}
