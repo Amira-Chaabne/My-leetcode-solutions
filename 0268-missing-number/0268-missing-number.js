526bceae413a1928cf23d8cd5ac23c7456e81af7
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-09-09 20:20:21 +0100

    Time: 89 ms (18.57%), Space: 44.4 MB (44.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,8 +3,25 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+  /** // Brut force solution
     const n = nums.length;
     const total = n * (n + 1) / 2;
     const sum = nums.reduce((a, b) => a + b, 0);
-    return total - sum;
+    return total - sum; */
+  
+  // Using Binary search
+    nums.sort((a, b) => a - b); 
+    let left = 0;
+    let right = nums.length; 
+    
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2); 
+        if (nums[mid] == mid) { 
+            left = mid + 1;  
+        } else {
+            right = mid; 
+        }
+    }
+  
+    return left;
 };
