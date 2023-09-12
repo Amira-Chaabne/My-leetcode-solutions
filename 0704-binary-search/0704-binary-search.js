79bf0a2b8139c77a4214da58f4fea39519901a69
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-09-12 08:32:55 +0100

    Time: 48 ms (91.93%), Space: 44.5 MB (89.02%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -5,10 +5,27 @@
  */
 var search = function(nums, target) {
   // Brut force solution ( with time complexity: O(n) )
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] === target) {
-            return i;
+    //for(let i = 0; i < nums.length; i++){
+        //if(nums[i] === target) {
+          //  return i;
+        //}
+    //}
+    //return -1;
+
+// Bineary search solution ( with time complexity: O(log n) )
+  let left = 0;
+    let right = nums.length - 1;
+
+    while(left <= right){
+        let m = Math.floor((left + right) / 2);
+        if(target === nums[m]) return m;
+
+        if(target < nums[m]){
+            right = m - 1;
+        } else {
+            left = m + 1;
         }
     }
+
     return -1;
 };
