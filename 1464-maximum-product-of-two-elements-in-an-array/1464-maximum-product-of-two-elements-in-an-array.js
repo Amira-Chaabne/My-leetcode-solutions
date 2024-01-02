06c96d86ec4fb26da296f28e35898541f312a8f0
Amira Chaabane <chaabaneamira.nan@gmail.com>
2024-01-02 22:54:02 +0100

    Time: 97 ms (5.13%), Space: 43.3 MB (39.34%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,8 +3,25 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    const sorted = nums.sort((a,b) => (a-b))
-    let i = sorted[sorted.length-1]
-    let j = sorted[sorted.length-2]
-    return (+i-1)*(+j-1)
-};
+   let biggest = 0;
+   let secondBiggest = 0;
+
+    for (let num of nums) {
+        if (num > biggest) {
+            secondBiggest = biggest;
+            biggest = num;
+        } else if (num > secondBiggest) {
+            secondBiggest = num;
+        }
+     }
+
+    return (biggest - 1) * (secondBiggest - 1);
+ }
+
+
+//var maxProduct = function(nums) {
+  // const sorted = nums.sort((a,b) => (a-b))
+  // let i = sorted[sorted.length-1]
+  // let j = sorted[sorted.length-2]
+  // return (+i-1)*(+j-1)
+//};
