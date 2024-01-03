2f31ccfe8f2f13622f05c4d1ee1b0c34b9a1109a
Amira Chaabane <chaabaneamira.nan@gmail.com>
2024-01-03 21:22:28 +0100

    Time: 73 ms (74.80%), Space: 46.7 MB (47.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,5 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    return nums.sort((a,b) => a === 0 ? 1 : b === 0 ? -1 : 0)
+   let nonZeroCount = 0;
+
+    // Move non-zero elements to the front
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[nonZeroCount] = nums[i];
+            nonZeroCount++;
+        }
+    }
+
+    // Fill the remaining positions with zeros
+    for (let i = nonZeroCount; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+    return nums;
+  
+  
+  // buit in sort solution
+  // return nums.sort((a,b) => a === 0 ? 1 : b === 0 ? -1 : 0)
 };
