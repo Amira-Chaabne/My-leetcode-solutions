ea8b8d9a08c51de5099954b35b4a2bc776188c64
Amira Chaabane <chaabaneamira.nan@gmail.com>
2024-01-18 08:51:27 +0100

    Time: 58 ms (22.91%), Space: 41.6 MB (62.20%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+   if (n <= 2) return n;
+
+    let oneStepBefore = 2;
+    let twoStepsBefore = 1;
+    let allWays = 0;
+
+    for (let i = 2; i < n; i++) {
+        allWays = oneStepBefore + twoStepsBefore;
+        twoStepsBefore = oneStepBefore;
+        oneStepBefore = allWays;
+    }
+
+    return allWays;
+};
